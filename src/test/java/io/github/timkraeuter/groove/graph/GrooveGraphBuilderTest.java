e74547fac9dfa8f3f22d635509419427d5e6cd76
Tim Kräuter <tim.kraeuterr@gmail.com>
2023-10-20 15:07:12 +0200

    Add test case

---
+++ src/test/java/io/github/timkraeuter/groove/graph/GrooveGraphBuilderTest.java
@@ -0,0 +1,20 @@
+package io.github.timkraeuter.groove.graph;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GrooveGraphBuilderTest {
+
+  @Test
+  void testBuilder() {
+    GrooveGraphBuilder builder = new GrooveGraphBuilder();
+    GrooveNode b = new GrooveNode("B");
+    GrooveNode a = new GrooveNode("A");
+    GrooveGraph graph =
+        builder.setName("Graph").addNode(a).addNode(b).addEdge("A to B", a, b).build();
+    assertEquals("Graph", graph.getName());
+    assertEquals(2, graph.nodes().count());
+    assertEquals(1, graph.edges().count());
+  }
+}
