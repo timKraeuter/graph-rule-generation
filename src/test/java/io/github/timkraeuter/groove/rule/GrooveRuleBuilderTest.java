6b1d3eecfb7900de592a00ba249b292b1a04a7ca
Tim Kräuter <tim.kraeuterr@gmail.com>
2023-10-31 15:49:46 +0100

    Rename test class

--- src/test/java/io/github/timkraeuter/groove/rule/GrooveRuleBuilderTest.java
+++ src/test/java/io/github/timkraeuter/groove/rule/GrooveRuleBuilderTest.java
@@ -14,7 +14,7 @@ import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class GrooveRuleGenerationTest {
+class GrooveRuleBuilderTest {
 
   @BeforeEach
   void setUp() {
